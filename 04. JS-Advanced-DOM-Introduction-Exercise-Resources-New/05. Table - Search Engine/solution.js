11c7801ed75625f9401ba5f1ae54349896147b3b
Ceco <c_vatovski@abv.bg>
2022-09-25 12:21:54 +0300

    Changes in the source

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,20 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
-
+   function onClick() {  
+      let arr = document.getElementsByTagName('tr');
+      for(let i = 0; i < arr.length; i++){
+         document.getElementsByTagName('tr')[i].classList.remove('select');
+      }
+      let arrText = [];
+      for(let  i = 2; i < arr.length; i++){  
+         arrText.push(arr[i].innerText.toLowerCase());
+      }
+      let textt = document.getElementById('searchField').value.toLowerCase();
+      for(let i = 0; i < arrText.length; i++){
+         if(arrText[i].includes(textt) && textt !== ''){
+            document.getElementsByTagName('tr')[i+2].classList.add('select');
+         }
+      }
    }
 }
