e8cb8809cf33fdcd8b9540da21f5f95bd12038d9
Ceco <c_vatovski@abv.bg>
2022-09-27 19:47:36 +0300

    Add new lesson and push some changes

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+    let text = document.getElementById('input').value.split('. ');
+    let output = document.getElementById('output');
+    while(output.firstChild){
+        output.removeChild(output.lastChild);
+    }
+    if(text[0] === '')
+        return;
+    for(let i = 0; i < text.length; i+=3){
+        let newText = '';
+        for(let j = 0; j < 3; j++){
+            newText += text[i+j] + '. ';
+        }
+        output.appendChild(document.createElement('p')).textContent = newText.trim();
+    }
 }
