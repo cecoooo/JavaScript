9541258694e91b9ba48b4558e183f5db7990f7fe
Ceco <c_vatovski@abv.bg>
2023-01-01 20:11:38 +0200

    First commit for 2023!

--- HappyNewYear.js
+++ HappyNewYear.js
@@ -1,4 +1,8 @@
 function HappyNewYear(){
+    console.log('31th Dec 2022');
+    console.log('Happy New Year!');
+    console.log('--------------')
+    console.log('1st Jan 2023');
     console.log('Happy New Year!');
 }
 
