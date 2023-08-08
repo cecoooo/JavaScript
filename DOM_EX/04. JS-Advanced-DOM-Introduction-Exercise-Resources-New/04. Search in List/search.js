739e61a5f272ce884861e684601608fce94f9feb
Ceco <c_vatovski@abv.bg>
2023-08-08 12:24:38 +0300

    mrss

--- DOM_EX/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ DOM_EX/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let listOfTowns = document.getElementsByTagName('li');
+   let currTown = document.getElementById('searchText').value;
+   let count = 0;
+   for(let i = 0; i < listOfTowns.length; i++){
+      if(listOfTowns[i].innerText.includes(currTown) && currTown !== ''){
+         document.getElementsByTagName('li')[i].style.fontWeight = 'bold';
+         document.getElementsByTagName('li')[i].style.textDecoration = 'underline';
+         count++;
+      }
+   }
+   if(count === 0){
+      for(let i = 0; i < listOfTowns.length; i++){
+         document.getElementsByTagName('li')[i].style.fontWeight = 'normal';
+         document.getElementsByTagName('li')[i].style.textDecoration = 'none';      
+      }
+   }
+   document.getElementById('result').innerText = count+' matches found';
 }
