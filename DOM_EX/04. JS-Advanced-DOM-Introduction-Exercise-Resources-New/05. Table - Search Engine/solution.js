1dc5aa54d7018aff4f3135d0f58e804a14198208
Ceco <c_vatovski@abv.bg>
2023-08-12 12:55:41 +0300

    dom neshto si

--- DOM_EX/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ DOM_EX/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      let names = document.getElementsByTagName('tr');
+      let input = document.getElementById('searchField').value;
+      for(let i = 2; i < names.length; i++){
+            document.getElementsByTagName('tr')[i].classList.remove('select');
+      }
+      for(let i = 2; i < names.length; i++){
+         if(names[i].innerText.includes(input) && input !== ''){
+            document.getElementsByTagName('tr')[i].classList.add('select');
+         }
+      }
    }
 }
