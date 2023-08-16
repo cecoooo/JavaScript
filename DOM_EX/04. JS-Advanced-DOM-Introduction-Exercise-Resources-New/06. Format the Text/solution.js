e7d7fd758d939ad96aa99a850717eb6d518f16aa
Ceco <c_vatovski@abv.bg>
2023-08-16 14:41:48 +0300

    mess

--- DOM_EX/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ DOM_EX/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+  let text = document.getElementById('input').value.split('.').filter(x => x !== '').map(x => x.trim()+'. ');
+  let output = document.getElementById('output');
+  while(output.firstChild){
+    output.removeChild(output.lastChild);
+  }
+  for(let i = 0; i < text.length; i+=3){
+    if(i+1 === text.length){
+      document.getElementById('output').appendChild(document.createElement('p')).textContent = text[i]; 
+      break; 
+    }
+    if(i+2 === text.length){
+      document.getElementById('output').appendChild(document.createElement('p')).textContent = text[i] 
+      + text[i+1];
+      break;  
+    }
+    output.appendChild(document.createElement('p')).textContent = text[i] 
+    + text[i+1] + text[i+2];
+  }
 }
