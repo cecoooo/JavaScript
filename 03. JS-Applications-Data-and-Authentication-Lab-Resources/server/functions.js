92eeb17dc1e8cea1c010bdd00a9589bbbb939164
Ceco <c_vatovski@abv.bg>
2023-09-08 16:55:40 +0300

    mess

---
+++ 03. JS-Applications-Data-and-Authentication-Lab-Resources/server/functions.js
@@ -0,0 +1,68 @@
+init();
+
+function init(){
+    document.getElementById('load').addEventListener('click', async () => {
+        const comments = await getComment();
+        displayComments(comments);
+    });
+    document.getElementById('send').addEventListener('click', onPost)
+    document.getElementById('comments').addEventListener('click', onCommentClick);
+    
+}
+
+async function onPost(){
+    const name = document.querySelector('[name="name"]').value;
+    const content = document.querySelector('[name="content"]').value;
+    await postComment({name, content});
+}
+
+function displayComments(comments){
+    const list = document.getElementById('comments');
+    list.replaceChildren(...comments.map(createCommentCard))
+}
+
+function createCommentCard(comment){
+    const element = document.createElement('article');
+    element.innerHTML = `<header><h3>${comment.name}</h3></header>
+    <main><p>${comment.content}</p>
+    <button id = 'delete'>Delete</button></main>`;
+    element.id = comment.id;
+    return element;
+}
+
+async function getComment(){
+    const respose = await fetch("http://localhost:3030/jsonstore/comments");
+    const data = await respose.json();
+    return Object.values(data);
+}
+
+async function postComment(comment){
+
+    const response = await fetch("http://localhost:3030/jsonstore/comments", {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment) 
+    });
+    const data = await response.json();
+    return data;
+}
+
+function onCommentClick(ev){
+    if(ev.target.tagName == 'BUTTON'){
+        const choice = confirm('Are you sure you want to delete this comment?');
+        if(choice){
+            const commentId = ev.target.parentElement.id;
+            deleteComment(commentId);
+        }
+    }
+}
+
+async function deleteComment(id){
+    const responce = await fetch("http://localhost:3030/jsonstore/comments/"+id,{
+        method: 'delete'
+    });
+
+    document.getElementById(id).remove();
+}
